d5e4a2b98de634c14d99dc7b2c24167cce6e4e1d
Fraser Louden <fraserlouden@outlook.com>
2020-07-28 16:00:51 +0100

    Update script.js
    Trying GSheetReader

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-fetch("https://sheetdb.io/api/v1/s6saou3ex6syc")
-  .then((response) => response.json())
-  .then((data) => {
+const GSheetReader = require('g-sheet-api');
 
-    var obj = data[0]; 
- document.getElementById("ncTracking").src=obj.URL; 
- 
-  })
-  .catch((error) => {
-    console.error(error);
+const SheetURL = '1PwvDFBJkQxlcAmzaYqmAWPAHjstb5Uqn72z-QL4KUmw'
+
+const options = {
+    sheetId: SheetURL,
+    sheetNumber: 1,
+    returnAllResults: true,
+    filter: {
+     // 'department': 'archaeology',
+     // 'module description': 'introduction'
+    },
+    filterOptions: {
+     // operator: 'or',
+     // matching: 'loose'
+    }
+  }
+
+GSheetReader(options, results => {
+    console.log(results)
   });
+
+
+//fetch("https://sheetdb.io/api/v1/s6saou3ex6syc")
+//  .then((response) => response.json())
+//  .then((data) => {
+
+//    var obj = data[0]; 
+// document.getElementById("ncTracking").src=obj.URL; 
+ 
+//  })
+//  .catch((error) => {
+//    console.error(error);
+//  });
